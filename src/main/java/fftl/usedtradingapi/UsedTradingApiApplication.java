5fe5ebbb5bc134ce66698ed9e27e647b7d9ff751
fftl <fftl.0710@gmail.com>
2022-02-05 21:22:48 +0900

    project init

---
+++ src/main/java/fftl/usedtradingapi/UsedTradingApiApplication.java
@@ -0,0 +1,13 @@
+package fftl.usedtradingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedTradingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedTradingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fftl/usedtradingapi/UsedTradingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fftl.usedtradingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedTradingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
