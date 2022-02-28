f42ef7a9e23805765cb924426536a4d4239bd6e2
fftl <fftl.0710@gmail.com>
2022-02-28 23:36:49 +0900

    update: gradle

--- src/test/java/fftl/usedtradingapi/user/service/UserServiceTest.java
+++ src/test/java/fftl/usedtradingapi/user/service/UserServiceTest.java
@@ -1,17 +1,28 @@
 package fftl.usedtradingapi.user.service;
 
+import fftl.usedtradingapi.user.domain.User;
+import fftl.usedtradingapi.user.domain.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
     @Test
     void saveUser() {
+        when(userRepository.save(any(User.class))).thenReturn("a");
     }
 
     @Test
