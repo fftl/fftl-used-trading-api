a314814083e313a9929fd7d15de02d7db104f5d1
fftl <fftl.0710@gmail.com>
2022-03-09 21:20:29 +0900

    update: jenkins test

--- src/test/java/fftl/usedtradingapi/UsedTradingApiApplicationTests.java
+++ src/test/java/fftl/usedtradingapi/UsedTradingApiApplicationTests.java
@@ -3,7 +3,7 @@ package fftl.usedtradingapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class UsedTradingApiApplicationTests {
 
 	@Test
