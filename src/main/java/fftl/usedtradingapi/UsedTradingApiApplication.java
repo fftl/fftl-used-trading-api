51f06110e7dc8f6c1644cd7cd6b80cc713a0c7c5
fftl <fftl.0710@gmail.com>
2022-03-11 23:57:34 +0900

    update: aws credentials update

--- src/main/java/fftl/usedtradingapi/UsedTradingApiApplication.java
+++ src/main/java/fftl/usedtradingapi/UsedTradingApiApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class UsedTradingApiApplication {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "classpath:aws.yml";
+            + "classpath:application.yml";
+//            + "classpath:aws.yml";
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(UsedTradingApiApplication.class)
