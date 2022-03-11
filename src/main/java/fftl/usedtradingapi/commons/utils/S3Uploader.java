6bd0132592cf122fd51654906f61bbc4aa44f4b5
fftl <fftl.0710@gmail.com>
2022-03-11 23:15:38 +0900

    update: aws credentials update

--- src/main/java/fftl/usedtradingapi/commons/utils/S3Uploader.java
+++ src/main/java/fftl/usedtradingapi/commons/utils/S3Uploader.java
@@ -3,7 +3,6 @@ package fftl.usedtradingapi.commons.utils;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import fftl.usedtradingapi.image.domain.ImageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
